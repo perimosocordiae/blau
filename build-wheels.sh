6c6f6d6e4f821a19be535137ef537bdb4271e2f8
CJ Carey <perimosocordiae@gmail.com>
2021-09-12 22:45:42 -0400

    Update wheel builder

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ex
 
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+curl -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
 cd /io
 
 for PYBIN in /opt/python/cp3{6,7,8,9,10}*/bin; do
+    rm -rf build/
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
     "${PYBIN}/python" setup.py bdist_wheel
 done
@@ -15,3 +16,8 @@ for whl in dist/*.whl; do
     auditwheel repair "$whl"
 done
 
+cd /
+for PYBIN in /opt/python/cp3{6,7,8,9,10}*/bin; do
+    "${PYBIN}/pip" install blau -f /io/wheelhouse/
+    "${PYBIN}/python" -c 'import blau; blau.BlauMove(0,1,2)'
+done
