f4b96c1834a76715466850ecc6abea16b879f4c8
CJ Carey <perimosocordiae@gmail.com>
2022-09-22 21:44:39 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import RustExtension, Binding
 
 setup(
     name='blau',
-    version='0.1.0',
+    version='0.1.1',
     author='CJ Carey',
     packages=['blau'],
     zip_safe=False,
