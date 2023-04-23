60f800d3c87e786c7793c0464ad4c312bed86afa
CJ Carey <perimosocordiae@gmail.com>
2023-04-22 23:13:57 -0400

    Make the python module an optional feature

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 from setuptools_rust import RustExtension, Binding
 
 setup(
-    name='blau',
-    version='0.1.1',
-    author='CJ Carey',
-    packages=['blau'],
+    name="blau",
+    version="0.1.2",
+    author="CJ Carey",
+    packages=["blau"],
     zip_safe=False,
     rust_extensions=[
-        RustExtension('blau.blau', debug=False, binding=Binding.RustCPython),
+        RustExtension(
+            "blau.blau", debug=False, binding=Binding.RustCPython, features=["cpython"]
+        ),
     ],
-    package_data=dict(blau=['blau.pyi']),
+    package_data=dict(blau=["blau.pyi"]),
 )
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@ pub mod colors;
 pub mod game_state;
 pub mod player_move;
 pub mod player_state;
+#[cfg(feature = "cpython")]
 mod python_module;
